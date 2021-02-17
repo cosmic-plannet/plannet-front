0b9e53a7d08bfd4a3e039d8770d9152924b30f8c
minjyo <minjyo8823@gmail.com>
2021-02-17 13:48:35 +0900

    update: Apply header design

--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { withRouter } from "react-router-dom";
 import styled from "styled-components";
+import logo from "../assets/slogo.png";
 
 const Wrapper = styled.div`
     width: 100%;
-    height: 70px;
-    border: 1px solid black;
+    height: 60px;
+    box-shadow: 0 2px 18px 0 rgba(0, 0, 0, 0.2);
     /* display: flex; */
     /* align-items: center; */
     text-align: center;
+    /* padding: 1rem; */
+    padding-top: 1rem;
+    padding-right: 1rem;
 `;
-// const Logo = styled.img``;
-const Logo = styled.div`
-    margin: auto 0;
+const Logo = styled.img`
+    width: 66px;
+    height: 35px;
+    /* margin-top: 15px; */
 `;
+
 const Profile = styled.div`
-    width: 50px;
-    height: 50px;
+    width: 40px;
+    height: 40px;
     border: 1px solid black;
     float: right;
-    margin: -10px 10px 0 0;
+    /* margin-right: 13px; */
+    /* margin: 13px 13px 13px 0; */
 `;
 
 const Header = ({ history }) => {
@@ -29,8 +36,8 @@ const Header = ({ history }) => {
 
     return (
         <Wrapper>
-            <Logo onClick={() => movePage("")}>로고</Logo>
-            <div onClick={() => movePage("login")}>로그인</div>
+            <Logo onClick={() => movePage("")} src={logo}></Logo>
+            {/* <div onClick={() => movePage("login")}>로그인</div> */}
             <Profile onClick={() => movePage("mypage")}></Profile>
         </Wrapper>
     );
