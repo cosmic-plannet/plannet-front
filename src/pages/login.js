47723331e47a1f7ab35525e6c5e8366c7e020c6f
minjyo <minjyo8823@gmail.com>
2021-02-17 13:17:56 +0900

    update: Apply login page design

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import logo from "../assets/logo.png";
+import plannet from "../assets/plannet.png";
 import { useDispatch, useSelector } from "react-redux";
 //import { getQuestion, getToday } from "store/box";
 
@@ -10,20 +12,34 @@ const Wrapper = styled.div`
     flex-direction: column;
     align-items: center;
 `;
-const Logo = styled.img``;
-
+const Logo = styled.img`
+    width: 126px;
+    height: 68px;
+    padding: 0.5rem 0;
+    padding-top: 30%;
+`;
+const Plannet = styled.img`
+    width: 283px;
+    height: 67px;
+`;
 const Title = styled.div`
-    font-size: 30px;
-    padding: 1rem 0;
+    font-size: 20px;
+    color: #212121;
+    padding: 2rem 0 1rem 0;
+`;
+const SubTitle = styled.div`
+    font-size: 20px;
+    color: #212121;
+    padding-bottom: 0.2rem;
 `;
 
-const SubTitle = styled.div``;
-
 const LoginBtn = styled.button`
-    border: 1px solid black;
-    width: 100px;
+    border: solid 5px #686196;
+    margin-top: 2rem;
+    width: 476px;
+    height: 60px;
     background-color: white;
-    margin-top: 1rem;
+    color: #686196;
 `;
 
 const Login = ({ history }) => {
@@ -33,10 +49,12 @@ const Login = ({ history }) => {
 
     return (
         <Wrapper>
-            <Logo></Logo>
+            <Logo src={logo}></Logo>
+            <Plannet src={plannet}></Plannet>
             <Title>나만의 온라인 스터디 플랫폼, PLAN:NET</Title>
-            <SubTitle>스터디에 참여하여 팀원들과 PLANET을 멋지게 성장시켜 보세요!</SubTitle>
-            <LoginBtn onClick={() => movePage("interest")}>구글 로그인</LoginBtn>
+            <SubTitle>스터디에 참여하여,</SubTitle>
+            <SubTitle>팀원들과 PLANET을 멋지게 성장시켜 보세요!</SubTitle>
+            <LoginBtn onClick={() => movePage("interest")}>구글 아이디로 로그인하기</LoginBtn>
         </Wrapper>
     );
 };
